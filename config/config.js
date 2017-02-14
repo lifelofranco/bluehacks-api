df410d11997f311f5d4deac92a7ba99d2a24cf17
Life Christian <lifelofranco@gmail.com>
2017-02-14 20:04:37 +0800

    Initial Commit

---
+++ config/config.js
@@ -0,0 +1,33 @@
+module.exports =  {
+    local : function()
+    {
+        return {
+            "host" : "127.0.0.1",
+            "port" : "27017",
+            "database" : "play",
+            "username" : "admin",
+            "password" : "123456"
+        };
+    },
+    staging : function()
+    {
+        return {
+            "host" : "ds151707.mlab.com",
+            "port" : "51707",
+            "database" : "play-db",
+            "username" : "play",
+            "password" : "play"
+        }
+    },
+    prod : function()
+    {
+        return {
+            "host" : "ds151707.mlab.com",
+            "port" : "51707",
+            "database" : "play-devs-prod",
+            "username" : "play",
+            "password" : "play"
+        };
+    }
+
+};
