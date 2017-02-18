a53a8b05e0d614bd50312bdefa33008569322228
Life Christian <lifelofranco@gmail.com>
2017-02-19 07:15:53 +0800

    fix typo

--- routes/application-route.js
+++ routes/application-route.js
@@ -76,7 +76,7 @@ module.exports = function(app, router, db, constants) {
         User.findOne({'_id': req.body.userId}, function(err, userClass){
 
             // _.each(appsClass, function(eachApp){
-              
+
           Scholarship.findOne({'_id':req.body.scholarshipId}, function(err, scholarshipClass){
 
             var scholarshipInfo ={
@@ -85,14 +85,14 @@ module.exports = function(app, router, db, constants) {
                 logo: scholarshipClass.logo,
                 description: scholarshipClass.description,
                 requirements: scholarshipClass.requirements,
-                files: scholarshipClass.files,
+                files: scholarshipClass.files
             };
 
             var appFinal = {
               status: req.body.status,
               scholarship: scholarshipInfo
             }
-            
+
             userClass.applications.push(appFinal);
             userClass.save(function(err){
               if (err) {
@@ -104,10 +104,10 @@ module.exports = function(app, router, db, constants) {
                       return res.json({ message : 'Application Registered!' });
                     }
             });
-            // return res.json(userClass.applications);                    
+            // return res.json(userClass.applications);
 
           });
-              
+
       });
   });
 
